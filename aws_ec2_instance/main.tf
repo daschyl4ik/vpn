29c4ef91219e05343fb06edbd58dcf7fe3ba5190
daschyl4ik <darya.kolganova1@gmail.com>
2022-07-11 04:04:24 +0600

    Created security group, attached to the instance

--- aws_ec2_instance/main.tf
+++ aws_ec2_instance/main.tf
@@ -13,12 +13,10 @@ terraform {
   required_version = ">= 0.14"
 }
 
-
 #launch instance
 resource "aws_instance" "main" {
   ami           = "ami-0440e5026412ff23f"     #free tier eligible, Ubuntu, 22.04 LTS, amd64
   instance_type = "t3.micro"                  #free tier eligible 
-  #subnet_id = "available after VPC is created"
 
   credit_specification {
     cpu_credits = "standard"
@@ -29,6 +27,10 @@ root_block_device {
   volume_size = 8
   iops = 3000
 }
+
+#security_groups - (Optional, EC2-Classic and default VPC only) 
+#A list of security group names to associate with.
+security_groups = [ "pet-project-vpn-sg" ]
 
 tags {
   Name = "pet-project-vpn-ec2"
