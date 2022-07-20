1cb7e8bd77743fde56ebeb438a3774207d36292d
daschyl4ik <darya.kolganova1@gmail.com>
2022-07-20 23:57:58 +0600

    Deleted ansible and pip3 from user data

--- aws_ec2/userdata.sh
+++ aws_ec2/userdata.sh
@@ -3,8 +3,6 @@
 sudo apt update && apt upgrade -y
 
 sudo apt install python3
-sudo apt -y install python3-pip
-sudo python3 -m pip install --user ansible
 
 sudo apt install ntpdate
 sudo apt install -y ntp
