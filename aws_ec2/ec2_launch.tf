ca5788906eff2cc45ed88b4bd712b11a29041067
daschyl4ik <darya.kolganova1@gmail.com>
2022-07-24 03:02:52 +0600

    SG added to EC2, deleted from vpc description

--- aws_ec2/ec2_launch.tf
+++ aws_ec2/ec2_launch.tf
@@ -1,14 +1,46 @@
+# find the VPC that matches the tags and gather all its data.
+data "aws_subnet" "public" {
+  tags = {
+    Name = "pet-project-vpn-public-subnet"
+  }
+}
+
+#launch instance
+resource "aws_instance" "main" {
+  ami           = "ami-0c9b2924fcd9b73a4"     #free tier eligible, Ubuntu, 22.04 LTS, amd64
+  instance_type = "t3.micro"                  #free tier eligible
+  subnet_id = data.aws_subnet.public.id
+
+  credit_specification {
+    cpu_credits = "standard"
+  }
+
+root_block_device {
+  volume_type = "gp3"
+  volume_size = 8
+  iops = 3000
+}
+
+tags = {
+  Name = "pet-project-vpn-ec2"
+}
+
+user_data = file("userdata.sh")
+
+}
+
+
+#creating security group
 resource "aws_security_group" "main" {
   name        = "pet-project-vpn-sg"
   description = "Allow my IP range"
-  vpc_id      = aws_vpc.main.id
 
   ingress {
     description      = "OpenVPN from my device - TCP"
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    cidr_blocks      = [var.my-beeline-subnet-cidr, var.my-tele2-subnet-cidr]
   }
 
 ingress {
